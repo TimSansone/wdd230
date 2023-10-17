c1e3ad4b6ca5cf91f6d3dee54ce78df12a39d9c9
TimSansone <96982699+TimSansone@users.noreply.github.com>
2023-10-17 00:09:10 -0400

    Update hamburger-menu.js

--- chamber/scripts/hamburger-menu.js
+++ chamber/scripts/hamburger-menu.js
@@ -1,7 +1,14 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+function toggleNav() {
+    var navigation = document.querySelector(".navigation");
+    var navButton = document.querySelector("#navButton");
+    
+    if (navigation.style.display === "flex") {
+        navigation.style.display = "none";
+        navButton.classList.remove("open");
+        navButton.classList.add("closed");
+    } else {
+        navigation.style.display = "flex";
+        navButton.classList.remove("closed");
+        navButton.classList.add("open");
+    }
+}

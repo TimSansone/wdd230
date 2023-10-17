e6a3598f357cf3f3b9b49654a1c176a35b2e251c
TimSansone <96982699+TimSansone@users.noreply.github.com>
2023-10-17 07:55:25 -0400

    Add files via upload

--- chamber/scripts/hamburger-menu.js
+++ chamber/scripts/hamburger-menu.js
@@ -1,14 +1,7 @@
-function toggleNav() {
-    var navigation = document.querySelector(".navigation");
-    var navButton = document.querySelector("#navButton");
-    
-    if (navigation.style.display === "flex") {
-        navigation.style.display = "none";
-        navButton.classList.remove("open");
-        navButton.classList.add("closed");
-    } else {
-        navigation.style.display = "flex";
-        navButton.classList.remove("closed");
-        navButton.classList.add("open");
-    }
-}
+const mainmenu = document.querySelector('.nav-menu');
+const hambutton = document.querySelector('#navButton');
+
+hambutton.addEventListener('click', () => {
+	mainmenu.classList.toggle('open');
+	hambutton.classList.toggle('open');
+});

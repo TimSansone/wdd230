2df04bbf25baee726d1d4881a09974485e55458a
TimSansone <96982699+TimSansone@users.noreply.github.com>
2023-10-19 16:45:24 -0400

    Week 6 updates

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,26 @@
+// Function to calculate wind chill
+function calculateWindChill(temperatureFahrenheit, windSpeedMilesPerHour) {
+    if (temperatureFahrenheit >= 50 || windSpeedMilesPerHour < 3) {
+        return "N/A";
+    } else {
+        const windChill = 35.74 + 0.6215 * temperatureFahrenheit - 35.75 * Math.pow(windSpeedMilesPerHour, 0.16) + 0.4275 * temperatureFahrenheit * Math.pow(windSpeedMilesPerHour, 0.16);
+        return windChill.toFixed(1); // Round to one decimal place
+    }
+}
+
+// Retrieve temperature and wind speed from the weather.js script
+const temperatureFahrenheit = parseFloat(document.getElementById('temperature').textContent.split(' ')[1].slice(0, -2));
+const windSpeedMilesPerHour = parseFloat(document.getElementById('wind-speed').textContent.split(' ')[2]);
+
+function calculateWindChill(temperatureFahrenheit, windSpeedMilesPerHour) {
+    if (temperatureFahrenheit >= 50 || windSpeedMilesPerHour < 3) {
+        document.getElementById('wind-chill').textContent = `Wind Chill: N/A`;
+    } else {
+        const windChill = 35.74 + 0.6215 * temperatureFahrenheit - 35.75 * Math.pow(windSpeedMilesPerHour, 0.16) + 0.4275 * temperatureFahrenheit * Math.pow(windSpeedMilesPerHour, 0.16);
+        document.getElementById('wind-chill').textContent = `Wind Chill: ${windChill}°F`;
+    }
+}
+
+// Calculate wind chill and update the HTML element
+/* const windChill = calculateWindChill(temperatureFahrenheit, windSpeedMilesPerHour);
+document.getElementById('wind-chill').textContent = `Wind Chill: ${windChill}°F`; */

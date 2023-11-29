d496e35878ebb2ada801861ceba588b7dd6afbe0
TimSansone <96982699+TimSansone@users.noreply.github.com>
2023-11-29 03:38:22 -0500

    Update index.js

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Check whether it's Monday or Tuesday to show banner
     const day = dateObj.getDay();
-    if(day > 0 && day < 3) { 
+    if(day > 0 && day < 4) { 
         // Create container for banner
         let banner = document.createElement("section");
         banner.classList.add("banner");

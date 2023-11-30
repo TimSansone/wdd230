5751de77f8f49a797b92cc0c12b91f2856f8acab
TimSansone <96982699+TimSansone@users.noreply.github.com>
2023-11-30 00:57:30 -0500

    Update weather.js

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,76 +1,52 @@
-// Define constants for API key and city outside the functions
-const apiKey = '5a8231c5a5421807f22f48fd534cb44d'; // Replace with your OpenWeatherMap API key
-const city = 'Quezon City'; // Replace with the desired city name
-
-// Function to get current weather data
-export async function getWeatherData() {
-    const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=28.54&lon=-81.38&units=imperial&appid=5a8231c5a5421807f22f48fd534cb44d';
+// Function to fetch weather data from the API
+async function getWeatherData() {
+    const apiKey = '5a8231c5a5421807f22f48fd534cb44d';
+    const city = 'Orlando, Florida';
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=28.54&lon=-81.38&units=imperial&appid=5a8231c5a5421807f22f48fd534cb44d`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=28.54&lon=-81.38&units=imperial&appid=5a8231c5a5421807f22f48fd534cb44d`;
 
     try {
         const response = await fetch(weatherUrl);
         const data = await response.json();
-
         const temperature = data.main.temp;
-        const windSpeed = data.wind.speed;
-        const humidity = data.main.humidity;
-        const iconCode = data.weather[0].icon;
+        const description = data.weather[0].description;
 
-        const windChillIndex = 5.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+        document.getElementById('city').textContent += city;
+        document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
+        document.getElementById('description').textContent = `Description: ${description}`;
+        document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 
-        // Use const for element references
-        const descriptionElement = document.getElementById('description');
-        const cityElement = document.getElementById('city');
-        const temperatureElement = document.getElementById('temperature');
-        const windChillElement = document.getElementById('windChill');
-        const humidityElement = document.getElementById('humidity');
-        const weatherIconElement = document.getElementById('weather-icon');
+        const forecastResponse = await fetch(forecastUrl);
+        const forecastData = await forecastResponse.json();
+        const threeDayForecast = forecastData.list.slice(0, 3);
 
-        descriptionElement.innerText = `Status: ${data.weather[0].description}`;
-        cityElement.textContent = city;
-        temperatureElement.textContent = `${temperature} °F`;
-        windChillElement.textContent = `Chill: ${windChillIndex.toFixed(2)} °F`;
-        humidityElement.textContent = `Report: ${humidity}%`;
+        const forecastContainer = document.getElementById('forecast-container');
+        forecastContainer.innerHTML = '';
 
-        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-        weatherIconElement.src = iconUrl;
+        threeDayForecast.forEach(entry => {
+            const forecastTemperature = entry.main.temp;
+            const weatherIcon = entry.weather[0].icon;
+
+            const forecastItem = document.createElement('div');
+            forecastItem.classList.add('forecast-item');
+
+            const temperatureText = document.createTextNode(`Temperature: ${forecastTemperature}°C`);
+            forecastItem.appendChild(temperatureText);
+            forecastItem.style.color = '#060000';
+
+            const iconImage = document.createElement('img');
+            iconImage.src = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
+            // Set the width and height of the image to 15px
+            iconImage.style.width = '25px';
+            iconImage.style.height = '25px';
+
+            forecastItem.appendChild(iconImage);
+            forecastContainer.appendChild(forecastItem);
+        });
     } catch (error) {
-        console.error('Error fetching weather data: ', error);
-        // Set N/A for all elements in case of an error
-        document.getElementById('city').textContent = 'City: N/A';
-        document.getElementById('temperature').textContent = 'Temperature: N/A';
-        document.getElementById('windChill').textContent = 'Wind Chill: N/A';
-        document.getElementById('humidity').textContent = 'Humidity: N/A';
-        document.getElementById('weather-icon').src = ''; // Clear the icon
+        console.error('Error fetching weather data', error);
     }
 }
 
-// Function to get weather forecast
-export async function getWeatherForecast() {
-    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=28.54&lon=-81.38&units=imperial&appid=5a8231c5a5421807f22f48fd534cb44d';
-
-    try {
-        const response = await fetch(forecastUrl);
-        const data = await response.json();
-
-        const forecastList = data.list;
-
-        const day1 = forecastList[0];
-        const day2 = forecastList[8];
-        const day3 = forecastList[16];
-
-        // Use const for element references
-        const day1Element = document.getElementById('day1');
-        const day2Element = document.getElementById('day2');
-        const day3Element = document.getElementById('day3');
-
-        day1Element.textContent = `Tomorrow: ${day1.main.temp} °F`;
-        day2Element.textContent = `Next Day: ${day2.main.temp} °F`;
-        day3Element.textContent = `Other Day: ${day3.main.temp} °F`;
-    } catch (error) {
-        console.error('Error fetching weather forecast: ', error);
-    }
-}
-
-// Call functions after defining them
+// Call the function to fetch weather data and update UI
 getWeatherData();
-getWeatherForecast();
